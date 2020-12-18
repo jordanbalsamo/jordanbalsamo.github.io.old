3e2bb2d15ab4c63b5e6d57331e9c7f439f3f9657
Jordan Balsamo <jordanbalsamo@Jordans-MacBook-Pro.local>
2020-12-18 19:14:50 +0000

    Updates

---
+++ precache-manifest.40c71b7e240fad36b1f8656ed33de557.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5349027ebbdfb537bbfef81cce9838e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52c5aabbb2ce4118955f",
+    "url": "/static/css/2.3a395901.chunk.css"
+  },
+  {
+    "revision": "dfcd41c266d15a83d486",
+    "url": "/static/css/main.00770f9f.chunk.css"
+  },
+  {
+    "revision": "52c5aabbb2ce4118955f",
+    "url": "/static/js/2.7e84832b.chunk.js"
+  },
+  {
+    "revision": "3126d86a50e9ed9efd2cb0b980b65602",
+    "url": "/static/js/2.7e84832b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcd41c266d15a83d486",
+    "url": "/static/js/main.385b841e.chunk.js"
+  },
+  {
+    "revision": "8b9a82857dcebd3a1747",
+    "url": "/static/js/runtime-main.92e2db11.js"
+  },
+  {
+    "revision": "ce63c0459a272d8301b186ec73db37e7",
+    "url": "/static/media/check-circle.ce63c045.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "e50a37ab483761bdecb44a3c48c7455f",
+    "url": "/static/media/logo.e50a37ab.png"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "1de1406c7d731eac5d1fe0c638eab549",
+    "url": "/static/media/quotes-l.1de1406c.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/static/media/x.b0200bf7.svg"
+  }
+]);
